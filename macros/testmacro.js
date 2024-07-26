d83337e1b71a53acee3e5db89298371b47d751f8
Oli Lan <oli@olilan.co.uk>
2024-07-26 18:43:53 +0100

    Trying text id

--- macros/testmacro.js
+++ macros/testmacro.js
@@ -1,3 +1,3 @@
-/* {"name":"Test Macro","img":"systems/pf2e/icons/equipment/alchemical-items/alchemical-bombs/acid-flask.webp","_id":"SaXHztUmj7Fh9G59"} */
+/* {"name":"Test Macro","img":"systems/pf2e/icons/equipment/alchemical-items/alchemical-bombs/acid-flask.webp","_id":"testmacro"} */
 
 console.log("Hello!");
