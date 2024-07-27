990225be2363c7f6950bbcf538509a3cc9ff8f8a
Oli Lan <mail@olilan.co.uk>
2024-07-27 15:32:56 +0100

    Updating text

--- macros/testmacro.js
+++ macros/testmacro.js
@@ -1,4 +1,4 @@
 /* {"name":"Test Macro","img":"systems/pf2e/icons/equipment/alchemical-items/alchemical-bombs/acid-flask.webp","_id":"J73Nx18gXT9VUOXM"} */
 
 console.log("Hello!");
-console.log("Not for prod!");
+console.log("Here's a change");
