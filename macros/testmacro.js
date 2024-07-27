043ba7969390367d0d8c760c8013eedb0122fd4a
Oli Lan <mail@olilan.co.uk>
2024-07-27 15:26:37 +0100

    A test working change, not for prod

--- macros/testmacro.js
+++ macros/testmacro.js
@@ -1,3 +1,4 @@
 /* {"name":"Test Macro","img":"systems/pf2e/icons/equipment/alchemical-items/alchemical-bombs/acid-flask.webp","_id":"J73Nx18gXT9VUOXM"} */
 
 console.log("Hello!");
+console.log("Not for prod!");
