6a3af78e5457cab8823a610d5d8b6b38ccf0dcc1
Oli Lan <mail@olilan.co.uk>
2024-08-08 20:11:54 +0100

    Further tidying

--- src/soundbytrait.js
+++ src/soundbytrait.js
@@ -1,46 +1,51 @@
 import hitSoundsDatabase from "../databases/hit_sounds_database.json" with { type: "json" };
 
+const HIT_SOUND_VOLUME = 0.7;
+
 export function onDamageTaken(flags) {
-  const myRollOptions = flags.pf2e.rollOptions.all;
-  const myTraits = extractTraits(myRollOptions)
-  const myBestMatch = findBestMatch(myTraits);
-  console.log(myBestMatch);
-  const returnedSounds = myBestMatch.files;
+  const rollOptions = flags.pf2e.rollOptions.all;
+  const bestMatch = findBestMatch(extractTraits(rollOptions));
+  
+  const returnedSounds = bestMatch.files;
   const randomIndex = Math.floor(Math.random() * returnedSounds.length);
-  const soundSelected = returnedSounds[randomIndex];
-  AudioHelper.play({
-      src: soundSelected,
-      volume: 0.7,
-      autoplay: true,
-      loop: false
-  }, true);
+
+  playSound(returnedSounds[randomIndex]);
 }
 
 function findBestMatch(traits) {
-    let bestMatch = null;
-    let maxMatchingTraits = 0;
-  
-    for (const entry of hitSoundsDatabase) {
-      const matchingTraits = entry.traits.filter(trait => traits.includes(trait)).length;
-  
-      if (matchingTraits > maxMatchingTraits) {
-        bestMatch = entry;
-        maxMatchingTraits = matchingTraits;
-      }
+  let bestMatch = null;
+  let maxMatchingTraits = 0;
+
+  for (const entry of hitSoundsDatabase) {
+    const matchingTraits = entry.traits.filter(trait => traits.includes(trait)).length;
+
+    if (matchingTraits > maxMatchingTraits) {
+      bestMatch = entry;
+      maxMatchingTraits = matchingTraits;
     }
-  
-    return bestMatch;
   }
 
+  return bestMatch;
+}
+
 function extractTraits(obj) {
-    const traits = [];
-  
-    for (const key in obj) {
-      if (key.startsWith("self:trait:")) {
-        const trait = key.replace("self:trait:", "");
-        traits.push(trait);
-      }
+  const traits = [];
+
+  for (const key in obj) {
+    if (key.startsWith("self:trait:")) {
+      const trait = key.replace("self:trait:", "");
+      traits.push(trait);
     }
-  
-    return traits;
   }
+
+  return traits;
+}
+
+function playSound(sound) {
+  AudioHelper.play({
+    src: soundSelected,
+    volume: HIT_SOUND_VOLUME,
+    autoplay: true,
+    loop: false
+  }, true);
+}
