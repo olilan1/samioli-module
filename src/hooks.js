f59e545ead25020df7fe3cd7b584288d6416a2ab
mustangsami <mustang.sami@gmail.com>
2024-08-11 13:25:51 +0300

    Actually update the import
    UGH

--- src/hooks.js
+++ src/hooks.js
@@ -1,4 +1,4 @@
-import {onDamageTaken} from "./soundbytrait.js"
+import {onDamageTaken} from "./soundbynameortrait.js"
 
 Hooks.on("updateActor", (arg0, arg1, arg2, arg3) => {
     if ("damageTaken" in arg2) {
