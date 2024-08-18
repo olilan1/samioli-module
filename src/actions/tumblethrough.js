c6ad21457fa9e4c57a8d14346f7733439c1236f7
mustangsami <mustang.sami@gmail.com>
2024-08-18 21:53:43 +0100

    Cleaned up conts in tumble through

--- src/actions/tumblethrough.js
+++ src/actions/tumblethrough.js
@@ -1,10 +1,17 @@
-export async function startTumbleThrough(ChatMessagePF2e) {
+const WOOSH_SOUND_1 = "sound/NWN2-Sounds/cb_sw_unarmed04.WAV";
+const WOOSH_SOUND_2 = "sound/NWN2-Sounds/cb_sw_unarmed01.WAV";
+const WOOSH_SOUND_3 = "sound/NWN2-Sounds/cb_sw_unarmed03.WAV";
+const WOOSH_SOUND_4 = "sound/NWN2-Sounds/cb_sw_unarmed1.WAV";
+const WOOSH_SOUND_5 = "sound/NWN2-Sounds/cb_sw_unarmed2.WAV";
+const WOOSH_SOUND_6 = "sound/NWN2-Sounds/cb_sw_unarmed02.WAV";
+const DEFLECT_SOUND = "sound/NWN2-Sounds/bf_med_flesh.WAV";
+const LAND_SOUND = "sound/NWN2-Sounds/it_genericmedium.WAV";
 
+export async function startTumbleThrough(ChatMessagePF2e) {
+    
     //first check if the message is eligible
     if (!checkIfASkillCheck(ChatMessagePF2e)){
     } else {
-        console.log(`ChatMessagePF2e.flags.pf2e.context.options`, ChatMessagePF2e.flags.pf2e.context.options)
-
         //check if the skill check was for a tumblethrough
         if (!ChatMessagePF2e.flags.pf2e.context.options.includes("action:tumble-through")) {
             return
@@ -12,25 +19,25 @@ export async function startTumbleThrough(ChatMessagePF2e) {
         } else {
     
             //set up for the animations
-            const TOKENID = ChatMessagePF2e.speaker.token;
-            const TOKEN = canvas.tokens.placeables.find(t => t.id === TOKENID);
+            const tokenId = ChatMessagePF2e.speaker.token;
+            const token = canvas.tokens.placeables.find(t => t.id === tokenId);
     
-            const TARGETTOKENS = Array.from(game.user.targets)
-            if (TARGETTOKENS.length === 0) return;
+            const targetTokens = Array.from(game.user.targets)
+            if (targetTokens.length === 0) return;
     
-            const ORIGINALTOKENPOSITIONX = TOKEN.document.x;
-            const ORIGINALTOKEPOSITIONY = TOKEN.document.y;
-            const TARGETPOSITIONX = TARGETTOKENS[0].document.x;
-            const TARGETPOSITIONY = TARGETTOKENS[0].document.y;
-            const TARGETHEIGHT = TARGETTOKENS[0].document.height;
-            const TARGETLOCATIONBUFFER = game.canvas.scene.grid.size/2;
+            const originalTokenPositionX = token.document.x;
+            const OriginalTokenPositionY = token.document.y;
+            const targetPositionX = targetTokens[0].document.x;
+            const targetPositionY = targetTokens[0].document.y;
+            const targetHeight = targetTokens[0].document.height;
+            const TargetLocationBuffer = game.canvas.scene.grid.size/2;
     
-            let x = TARGETPOSITIONX - ORIGINALTOKENPOSITIONX
-            let y = TARGETPOSITIONY - ORIGINALTOKEPOSITIONY
+            let x = targetPositionX - originalTokenPositionX
+            let y = targetPositionY - OriginalTokenPositionY
     
-            for (let i = 1; i < TARGETHEIGHT; i++) {
-                x = x + TARGETLOCATIONBUFFER;
-                y = y + TARGETLOCATIONBUFFER;
+            for (let i = 1; i < targetHeight; i++) {
+                x = x + TargetLocationBuffer;
+                y = y + TargetLocationBuffer;
             }
 
             let rotationValue =  720
@@ -38,15 +45,6 @@ export async function startTumbleThrough(ChatMessagePF2e) {
             if (x < 0) {
                 rotationValue = rotationValue * -1
             }
-    
-            const WOOSHSOUND1 = "sound/NWN2-Sounds/cb_sw_unarmed04.WAV";
-            const WOOSHSOUND2 = "sound/NWN2-Sounds/cb_sw_unarmed01.WAV";
-            const WOOSHSOUND3 = "sound/NWN2-Sounds/cb_sw_unarmed03.WAV";
-            const WOOSHSOUND4 = "sound/NWN2-Sounds/cb_sw_unarmed1.WAV";
-            const WOOSHSOUND5 = "sound/NWN2-Sounds/cb_sw_unarmed2.WAV";
-            const WOOSHSOUND6 = "sound/NWN2-Sounds/cb_sw_unarmed02.WAV";
-            const DEFLECTSOUND = "sound/NWN2-Sounds/bf_med_flesh.WAV";
-            const LANDSOUND = "sound/NWN2-Sounds/it_genericmedium.WAV";
 
             //check if the skillroll was successful
             if (ChatMessagePF2e.flags.pf2e.context.outcome === "criticalSuccess"
@@ -57,56 +55,56 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                 let sequence = new Sequence({moduleName: "PF2e Animations", softFail: true})
                 
                 .animation()
-                    .on(TOKEN)
+                    .on(token)
                     .opacity(0)
                     .fadeIn(200)
                     .duration(ANIMATIONTIME)
                 .sound()
-                    .file(WOOSHSOUND1, true, true)
+                    .file(WOOSH_SOUND_1, true, true)
                     .delay(500)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND1);
+                        return fileExistsAtPath(WOOSH_SOUND_1);
                     })
                 .sound()
-                    .file(WOOSHSOUND2, true, true)
+                    .file(WOOSH_SOUND_2, true, true)
                     .delay(900)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND2);
+                        return fileExistsAtPath(WOOSH_SOUND_2);
                     })
                 .sound()
-                    .file(WOOSHSOUND3, true, true)
+                    .file(WOOSH_SOUND_3, true, true)
                     .delay(1300)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND3);
+                        return fileExistsAtPath(WOOSH_SOUND_3);
                     })
                 .sound()
-                    .file(WOOSHSOUND4, true, true)
+                    .file(WOOSH_SOUND_4, true, true)
                     .delay(2700)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND4);
+                        return fileExistsAtPath(WOOSH_SOUND_4);
                     })
                 .sound()
-                    .file(WOOSHSOUND5, true, true)
+                    .file(WOOSH_SOUND_5, true, true)
                     .delay(3100)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND5);
+                        return fileExistsAtPath(WOOSH_SOUND_5);
                     })
                 .sound()
-                    .file(WOOSHSOUND6, true, true)
+                    .file(WOOSH_SOUND_6, true, true)
                     .delay(3500)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND6);
+                        return fileExistsAtPath(WOOSH_SOUND_6);
                     })
                 .effect()
                     .file("jb2a.smoke.puff.side.02.white.0", true)
-                    .atLocation(TOKEN)
-                    .rotateTowards(TARGETTOKENS[0], {rotationOffset: 180})
+                    .atLocation(token)
+                    .rotateTowards(targetTokens[0], {rotationOffset: 180})
                     .scale(0.7)
                     .delay(600)
                     .fadeIn(100)
@@ -114,7 +112,7 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                     .fadeOut(200)
                 .effect()
                     .file("jb2a.smoke.puff.ring.01.white.0", true)
-                    .atLocation(TARGETTOKENS[0])
+                    .atLocation(targetTokens[0])
                     .scaleToObject(2.2)
                     .zIndex(1000)
                     .delay(1000)
@@ -123,7 +121,7 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                     .fadeOut(200)
                 .effect()
                     .file("jb2a.smoke.puff.ring.01.white.1", true)
-                    .atLocation(TOKEN)
+                    .atLocation(token)
                     .belowTokens()
                     .scale(0.7)
                     .delay(3800)
@@ -131,7 +129,7 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                     .opacity(0.2)
                     .fadeOut(200)
                 .effect()
-                    .from(TOKEN)
+                    .from(token)
                     .loopProperty("sprite", "rotation", {
                         values: [0, rotationValue],
                         duration: ANIMATIONTIME/2,
@@ -155,7 +153,7 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                     .duration(ANIMATIONTIME)
                     .waitUntilFinished(-195)
                 .animation()
-                    .on(TOKEN)
+                    .on(token)
                     .opacity(1)
                     .fadeIn(200)
                     .duration(250)
@@ -164,12 +162,12 @@ export async function startTumbleThrough(ChatMessagePF2e) {
             } else if (ChatMessagePF2e.flags.pf2e.context.outcome === "criticalFailure" 
             || ChatMessagePF2e.flags.pf2e.context.outcome === "failure") {
 
-                const FALLDOWNX = (x/2)  
-                const FALLDOWNY = (y/2)  
+                const fallDownX = (x/2)  
+                const fallDownY = (y/2)  
 
-                const ROLLOUTANIMTIME = 2000
-                const KNOCKEDBACKANIMTIME = 1000
-                const FLATONFLOORTIME = 2000
+                const rollOutAnimTime = 2000
+                const knockedBackAnimTime = 1000
+                const FlatOnFloorTime = 2000
 
                 let fallFlatAngle = -90
 
@@ -180,58 +178,58 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                 let sequence = new Sequence({moduleName: "PF2e Animations", softFail: true})
                 
                 .animation()
-                    .on(TOKEN)
+                    .on(token)
                     .opacity(0)
                     .fadeIn(200)
-                    .duration(ROLLOUTANIMTIME + KNOCKEDBACKANIMTIME)
+                    .duration(rollOutAnimTime + knockedBackAnimTime)
                 .effect()
                     .file("jb2a.smoke.puff.side.02.white.0", true)
-                    .atLocation(TOKEN)
-                    .rotateTowards(TARGETTOKENS[0], {rotationOffset: 180})
+                    .atLocation(token)
+                    .rotateTowards(targetTokens[0], {rotationOffset: 180})
                     .scale(0.7)
-                    .delay(ROLLOUTANIMTIME / 2.2)
+                    .delay(rollOutAnimTime / 2.2)
                     .fadeIn(100)
                     .opacity(0.3)
                     .fadeOut(200)
                 .sound()
-                    .file(WOOSHSOUND1, true, true)
-                    .delay(ROLLOUTANIMTIME / 2.2)
+                    .file(WOOSH_SOUND_1, true, true)
+                    .delay(rollOutAnimTime / 2.2)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND1);
+                        return fileExistsAtPath(WOOSH_SOUND_1);
                     })
                 .sound()
-                    .file(WOOSHSOUND2, true, true)
-                    .delay(ROLLOUTANIMTIME / 1.5)
+                    .file(WOOSH_SOUND_2, true, true)
+                    .delay(rollOutAnimTime / 1.5)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND2);
+                        return fileExistsAtPath(WOOSH_SOUND_2);
                     })
                 .sound()
-                    .file(WOOSHSOUND3, true, true)
-                    .delay(ROLLOUTANIMTIME / 1.1)
+                    .file(WOOSH_SOUND_3, true, true)
+                    .delay(rollOutAnimTime / 1.1)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(WOOSHSOUND3);
+                        return fileExistsAtPath(WOOSH_SOUND_3);
                     })
                 .effect()
-                    .from(TOKEN)
-                    .animateProperty("sprite", "position.x", {from: 0, to: x, duration: ROLLOUTANIMTIME, ease: "easeInBack"})
-                    .animateProperty("sprite", "position.y", {from: 0, to: y, duration: ROLLOUTANIMTIME, ease: "easeInBack"})
-                    .animateProperty("sprite", "rotation", {from: 0, to: rotationValue, duration: ROLLOUTANIMTIME, ease: "easeInBack"})
-                    .duration(ROLLOUTANIMTIME)
+                    .from(token)
+                    .animateProperty("sprite", "position.x", {from: 0, to: x, duration: rollOutAnimTime, ease: "easeInBack"})
+                    .animateProperty("sprite", "position.y", {from: 0, to: y, duration: rollOutAnimTime, ease: "easeInBack"})
+                    .animateProperty("sprite", "rotation", {from: 0, to: rotationValue, duration: rollOutAnimTime, ease: "easeInBack"})
+                    .duration(rollOutAnimTime)
                     .zIndex(2)
                     .waitUntilFinished(0)
                 .effect()
                     .file("jb2a.smoke.puff.ring.01.white.0", true)
-                    .atLocation(TARGETTOKENS[0])
+                    .atLocation(targetTokens[0])
                     .scaleToObject(2.2)
                     .zIndex(1000)
                     .fadeIn(100)
                     .opacity(0.3)
                     .fadeOut(200)
                 .effect()
-                    .from(TARGETTOKENS[0])
+                    .from(targetTokens[0])
                     .loopProperty("sprite", "scale.x", {
                         values: [1, 1.3],
                         duration: 100,
@@ -248,41 +246,41 @@ export async function startTumbleThrough(ChatMessagePF2e) {
                     .duration(100)
                 .effect()
                     .file("jb2a.impact.008.orange", true)
-                    .atLocation(TARGETTOKENS[0])
+                    .atLocation(targetTokens[0])
                     .scaleToObject(3)
                     .zIndex(1000)
                 .sound()
-                    .file(DEFLECTSOUND, true, true)
+                    .file(DEFLECT_SOUND, true, true)
                     .fadeOutAudio(200)
                     .playIf(() => {
-                        return fileExistsAtPath(DEFLECTSOUND);
+                        return fileExistsAtPath(DEFLECT_SOUND);
                     })                   
                 .effect()
-                    .from(TOKEN)
-                    .animateProperty("sprite", "position.x", {from: x, to: FALLDOWNX, duration: KNOCKEDBACKANIMTIME, ease: "easeOutQuint"})
-                    .animateProperty("sprite", "position.y", {from: y, to: FALLDOWNY, duration: KNOCKEDBACKANIMTIME, ease: "easeOutQuint"})
-                    .animateProperty("sprite", "rotation", {from: 0, to: fallFlatAngle, duration: KNOCKEDBACKANIMTIME, ease: "easeOutQuint"})
-                    .duration(KNOCKEDBACKANIMTIME + FLATONFLOORTIME)
+                    .from(token)
+                    .animateProperty("sprite", "position.x", {from: x, to: fallDownX, duration: knockedBackAnimTime, ease: "easeOutQuint"})
+                    .animateProperty("sprite", "position.y", {from: y, to: fallDownY, duration: knockedBackAnimTime, ease: "easeOutQuint"})
+                    .animateProperty("sprite", "rotation", {from: 0, to: fallFlatAngle, duration: knockedBackAnimTime, ease: "easeOutQuint"})
+                    .duration(knockedBackAnimTime + FlatOnFloorTime)
                     .zIndex(2)
                     .fadeOut(500)
                 .effect()
                     .file("jb2a.smoke.puff.ring.01.white.1", true)
-                    .atLocation({x: ORIGINALTOKENPOSITIONX + (game.canvas.grid.size/2) + FALLDOWNX, y: ORIGINALTOKEPOSITIONY + (game.canvas.grid.size/2) + FALLDOWNY})
+                    .atLocation({x: originalTokenPositionX + (game.canvas.grid.size/2) + fallDownX, y: OriginalTokenPositionY + (game.canvas.grid.size/2) + fallDownY})
                     .scale(0.8)
                     .fadeIn(100)
                     .opacity(0.5)
                     .fadeOut(200)
                     .zIndex(1)
-                    .delay(KNOCKEDBACKANIMTIME - 500)
+                    .delay(knockedBackAnimTime - 500)
                 .sound()
-                    .file(LANDSOUND, true, true)
+                    .file(LAND_SOUND, true, true)
                     .playIf(() => {
-                        return fileExistsAtPath(LANDSOUND);
+                        return fileExistsAtPath(LAND_SOUND);
                     })
-                    .delay(KNOCKEDBACKANIMTIME - 500)
+                    .delay(knockedBackAnimTime - 500)
                 .animation()
-                    .on(TOKEN)
-                    .delay(KNOCKEDBACKANIMTIME + FLATONFLOORTIME)
+                    .on(token)
+                    .delay(knockedBackAnimTime + FlatOnFloorTime)
                     .opacity(1)
                     .fadeIn(500)
             sequence.play()
