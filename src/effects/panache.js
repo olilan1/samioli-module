72e8da419371267e549e09dcef6b5f951be3d9d0
mustangsami <mustang.sami@gmail.com>
2024-09-01 16:17:25 -0400

    Auto remove panache on missed finishers or finisher damage rolls

--- src/effects/panache.js
+++ src/effects/panache.js
@@ -1,36 +1,52 @@
-export function checkForBravado(ChatMessagePF2e) {
-    if (ChatMessagePF2e.flags?.pf2e?.context?.options.includes("item:trait:bravado")) {
-        console.log("Detected a skill check or action with the bravado trait")
-        checkIfProvidesPanache(ChatMessagePF2e)
-    } else{
-        return;
-    }
+export function checkForBravado(chatMessage) {
+    if (chatMessage.flags?.pf2e?.context?.options.includes("item:trait:bravado")) {
+        checkIfProvidesPanache(chatMessage);
+    } 
 }
 
-function checkIfProvidesPanache(ChatMessagePF2e) {
-    if (ChatMessagePF2e.flags.pf2e.context.outcome === "criticalSuccess" 
-    || ChatMessagePF2e.flags.pf2e.context.outcome === "success" ) {
-        console.log("success means unlimited panache")
-        applyPanache(game.actors.get(ChatMessagePF2e.speaker.actor))
-    } else if (ChatMessagePF2e.flags.pf2e.context.outcome === "failure") {
-        console.log("failure means panache but only until the end of your next turn")
-        applyPanache(game.actors.get(ChatMessagePF2e.speaker.actor))
-    } else {
-        console.log("critical failure means no panache")
-    }
+function checkIfProvidesPanache(chatMessage) {
+    const outcome = chatMessage.flags.pf2e.context.outcome;
+    if (outcome === "criticalSuccess" || outcome === "success" || outcome === "failure") {
+        applyPanache(game.actors.get(chatMessage.speaker.actor));
+        if (outcome === "failure") {
+            console.log("failure means panache but only until the end of your next turn");
+            //TODO create a new panache item effect that lasts only one turn
+        }
+    } 
 }
 
 async function applyPanache(actor) {
-    const panacheItemId = "uBJsxCzNhje8m8jj"
-    const compendiumPack = game.packs.get("pf2e.feat-effects")
+    const panacheItemId = "uBJsxCzNhje8m8jj";
+    const compendiumPack = game.packs.get("pf2e.feat-effects");
     const panacheEffect = await compendiumPack.getDocument(panacheItemId);
-    const panacheEffectObject = panacheEffect.toObject()
-
-    await delay(4000)
-
-    await actor.createEmbeddedDocuments("Item", [panacheEffectObject]);
+    await delay(4000);
+    await actor.createEmbeddedDocuments("Item", [panacheEffect.toObject()]);
 }
 
 function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
-  }
+}
+
+export async function checkForFinisher(chatMessage) {
+    const { flags } = chatMessage;
+    const hasFinisher = flags?.pf2e?.context?.options.includes("finisher");
+    const isDamageRoll = flags?.pf2e?.context?.type === "damage-roll";
+    const isFailure = flags?.pf2e?.context?.outcome === "failure" 
+    || flags?.pf2e?.context?.outcome === "criticalFailure";
+
+    if ((hasFinisher && isDamageRoll) || (hasFinisher && isFailure)) {
+        const panacheItems = await returnPanacheItems(game.actors.get(chatMessage.speaker.actor));
+        if (panacheItems.length > 0) {
+            await delay(4000);
+            for (const panacheItem of panacheItems) {
+                await panacheItem.delete(); 
+            }
+        } 
+    } 
+}
+
+async function returnPanacheItems(actor) {
+    const items = await actor.items.contents;
+    return items.filter(item => item.type === "effect" 
+        && item.system.slug === "effect-panache"); 
+}
--- src/hooks.js
+++ src/hooks.js
@@ -3,7 +3,7 @@ import { creatureSoundOnDamage, creatureSoundOnAttack } from "./creaturesounds.j
 import { chatMacroButton } from "./chatmacrobutton.js";
 import { startTumbleThrough } from "./actions/tumblethrough.js";
 import { startEnjoyTheShow } from "./actions/enjoytheshow.js";
-import { checkForBravado } from "./effects/panache.js";
+import { checkForBravado, checkForFinisher } from "./effects/panache.js";
 
 Hooks.on("init", () => {
     registerSettings();
@@ -17,7 +17,8 @@ Hooks.on("createChatMessage", (ChatMessagePF2e, rollmode, id) => {
     creatureSoundOnAttack(ChatMessagePF2e);
     startTumbleThrough(ChatMessagePF2e);
     startEnjoyTheShow(ChatMessagePF2e);
-    checkForBravado(ChatMessagePF2e)
+    checkForBravado(ChatMessagePF2e);
+    checkForFinisher(ChatMessagePF2e);
 });
 
 Hooks.on('renderChatMessage', async (ChatMessagePF2e, html) => {
