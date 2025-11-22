27b10f09319e935af875cb47a4235438cb60b9af
mustangsami <mustang.sami@gmail.com>
2025-11-22 12:25:58 +0100

    spaces

--- src/sockets.ts
+++ src/sockets.ts
@@ -7,7 +7,7 @@ let socket: SocketlibSocket | undefined;
 
 export const MANIFEST_EIDOLON = "manifestEidolon";
 export const DEMANIFEST_EIDOLON = "demanifestEidolon";
-export const  REMOVE_AND_APPLY_HUNT_PREY = "removeAndApplyHuntPrey";
+export const REMOVE_AND_APPLY_HUNT_PREY = "removeAndApplyHuntPrey";
 export const DAZZLING_DISPLAY = "dazzlingDisplay";
 
 export const getSocket = () => {
@@ -20,6 +20,6 @@ export function registerSocket() {
     // Register the name of the function that you want to run as the GM
     socket.register(MANIFEST_EIDOLON, manifestEidolonAsGM);
     socket.register(DEMANIFEST_EIDOLON, demanifestEidolonAsGM);
-    socket.register( REMOVE_AND_APPLY_HUNT_PREY, removeAndApplyHuntPreyAsGM);
+    socket.register(REMOVE_AND_APPLY_HUNT_PREY, removeAndApplyHuntPreyAsGM);
     socket.register(DAZZLING_DISPLAY, startDazzlingDisplayAsGM);
 }
