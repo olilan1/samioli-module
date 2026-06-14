8235a29938a648c36e02a7562c45c3bf41fc0999
mustangsami <mustang.sami@gmail.com>
2026-06-14 14:03:36 +0100

    formatting issue

--- src/actions/tumblethrough.ts
+++ src/actions/tumblethrough.ts
@@ -102,97 +102,97 @@ async function animateFailureTumble(x: number, y: number, token: TokenPF2e, targ
 
     const sequence = new Sequence()
         .animation()
-        .on(token)
-        .opacity(0)
-        .fadeIn(200)
-        .duration(rollOutAnimTime + knockedBackAnimTime)
-        .effect()
-        .file(puffSideAnimation)
-        .atLocation(token)
-        // @ts-expect-error rotationOffset is correct
-        .rotateTowards(target, { rotationOffset: 180 })
-        .scale(0.7)
-        .delay(rollOutAnimTime / 2.2)
-        .fadeIn(100)
-        .opacity(0.3)
-        .fadeOut(200)
+            .on(token)
+            .opacity(0)
+            .fadeIn(200)
+            .duration(rollOutAnimTime + knockedBackAnimTime)
+            .effect()
+            .file(puffSideAnimation)
+            .atLocation(token)
+            // @ts-expect-error rotationOffset is correct
+            .rotateTowards(target, { rotationOffset: 180 })
+            .scale(0.7)
+            .delay(rollOutAnimTime / 2.2)
+            .fadeIn(100)
+            .opacity(0.3)
+            .fadeOut(200)
+            .sound()
+            .file(wooshSound1)
+            .delay(rollOutAnimTime / 2.2)
+            .fadeOutAudio(200)
         .sound()
-        .file(wooshSound1)
-        .delay(rollOutAnimTime / 2.2)
-        .fadeOutAudio(200)
+            .file(wooshSound2)
+            .delay(rollOutAnimTime / 1.5)
+            .fadeOutAudio(200)
+            .sound()
+            .file(wooshSound3)
+            .delay(rollOutAnimTime / 1.1)
+            .fadeOutAudio(200)
+            .effect()
+            .copySprite(token)
+            .animateProperty("sprite", "position.x", { from: 0, to: x, duration: rollOutAnimTime, ease: "easeInBack" })
+            .animateProperty("sprite", "position.y", { from: 0, to: y, duration: rollOutAnimTime, ease: "easeInBack" })
+            .animateProperty("sprite", "rotation", { from: 0, to: rotationValue, duration: rollOutAnimTime, ease: "easeInBack" })
+            .duration(rollOutAnimTime)
+            .zIndex(2)
+            .waitUntilFinished(0)
+        .effect()
+            .file(puffRingAnimation1)
+            .atLocation(target)
+            .scaleToObject(2.2)
+            .zIndex(1000)
+            .fadeIn(100)
+            .opacity(0.3)
+            .fadeOut(200)
+        .effect()
+            .copySprite(target)
+            .loopProperty("sprite", "scale.x", {
+                values: [1, 1.3],
+                duration: 100,
+                pingPong: true,
+                ease: "easeInExpo"
+            })
+            .loopProperty("sprite", "scale.y", {
+                values: [1, 1.3],
+                duration: 100,
+                pingPong: true,
+                gridUnits: false,
+                ease: "easeInExpo"
+            })
+            .duration(100)
+        .effect()
+            .file(impactAnimation)
+            .atLocation(target)
+            .scaleToObject(3)
+            .zIndex(1000)
+            .sound()
+            .file(deflectSound)
+            .fadeOutAudio(200)
+        .effect()
+            .copySprite(token)
+            .animateProperty("sprite", "position.x", { from: x, to: fallDownX, duration: knockedBackAnimTime, ease: "easeOutQuint" })
+            .animateProperty("sprite", "position.y", { from: y, to: fallDownY, duration: knockedBackAnimTime, ease: "easeOutQuint" })
+            .animateProperty("sprite", "rotation", { from: 0, to: fallFlatAngle, duration: knockedBackAnimTime, ease: "easeOutQuint" })
+            .duration(knockedBackAnimTime + flatOnFloorTime)
+            .zIndex(2)
+            .fadeOut(500)
+        .effect()
+            .file(puffRingAnimation2)
+            .atLocation({ x: originalTokenPositionX + (canvas.grid.size / 2) + fallDownX, y: originalTokenPositionY + (canvas.grid.size / 2) + fallDownY })
+            .scale(0.8)
+            .fadeIn(100)
+            .opacity(0.5)
+            .fadeOut(200)
+            .zIndex(1)
+            .delay(knockedBackAnimTime - 500)
         .sound()
-        .file(wooshSound2)
-        .delay(rollOutAnimTime / 1.5)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound3)
-        .delay(rollOutAnimTime / 1.1)
-        .fadeOutAudio(200)
-        .effect()
-        .copySprite(token)
-        .animateProperty("sprite", "position.x", { from: 0, to: x, duration: rollOutAnimTime, ease: "easeInBack" })
-        .animateProperty("sprite", "position.y", { from: 0, to: y, duration: rollOutAnimTime, ease: "easeInBack" })
-        .animateProperty("sprite", "rotation", { from: 0, to: rotationValue, duration: rollOutAnimTime, ease: "easeInBack" })
-        .duration(rollOutAnimTime)
-        .zIndex(2)
-        .waitUntilFinished(0)
-        .effect()
-        .file(puffRingAnimation1)
-        .atLocation(target)
-        .scaleToObject(2.2)
-        .zIndex(1000)
-        .fadeIn(100)
-        .opacity(0.3)
-        .fadeOut(200)
-        .effect()
-        .copySprite(target)
-        .loopProperty("sprite", "scale.x", {
-            values: [1, 1.3],
-            duration: 100,
-            pingPong: true,
-            ease: "easeInExpo"
-        })
-        .loopProperty("sprite", "scale.y", {
-            values: [1, 1.3],
-            duration: 100,
-            pingPong: true,
-            gridUnits: false,
-            ease: "easeInExpo"
-        })
-        .duration(100)
-        .effect()
-        .file(impactAnimation)
-        .atLocation(target)
-        .scaleToObject(3)
-        .zIndex(1000)
-        .sound()
-        .file(deflectSound)
-        .fadeOutAudio(200)
-        .effect()
-        .copySprite(token)
-        .animateProperty("sprite", "position.x", { from: x, to: fallDownX, duration: knockedBackAnimTime, ease: "easeOutQuint" })
-        .animateProperty("sprite", "position.y", { from: y, to: fallDownY, duration: knockedBackAnimTime, ease: "easeOutQuint" })
-        .animateProperty("sprite", "rotation", { from: 0, to: fallFlatAngle, duration: knockedBackAnimTime, ease: "easeOutQuint" })
-        .duration(knockedBackAnimTime + flatOnFloorTime)
-        .zIndex(2)
-        .fadeOut(500)
-        .effect()
-        .file(puffRingAnimation2)
-        .atLocation({ x: originalTokenPositionX + (canvas.grid.size / 2) + fallDownX, y: originalTokenPositionY + (canvas.grid.size / 2) + fallDownY })
-        .scale(0.8)
-        .fadeIn(100)
-        .opacity(0.5)
-        .fadeOut(200)
-        .zIndex(1)
-        .delay(knockedBackAnimTime - 500)
-        .sound()
-        .file(landSound)
-        .delay(knockedBackAnimTime - 500)
-        .animation()
-        .on(token)
-        .delay(knockedBackAnimTime + flatOnFloorTime)
-        .opacity(1)
-        .fadeIn(500);
+            .file(landSound)
+            .delay(knockedBackAnimTime - 500)
+            .animation()
+            .on(token)
+            .delay(knockedBackAnimTime + flatOnFloorTime)
+            .opacity(1)
+            .fadeIn(500);
     sequence.play();
     await delay(rollOutAnimTime + flatOnFloorTime + knockedBackAnimTime);
 }
@@ -203,95 +203,95 @@ async function animateSuccessfulTumble(token: TokenPF2e, target: TokenPF2e, rota
     const sequence = new Sequence()
 
         .effect()
-        .copySprite(token)
-        .duration(500)
-        .waitUntilFinished(-450)
+            .copySprite(token)
+            .duration(500)
+            .waitUntilFinished(-450)
+            .animation()
+            .on(token)
+            .opacity(0)
+            .fadeIn(200)
+            .duration(animationTime)
+        .sound()
+            .file(wooshSound1)
+            .delay(500)
+            .fadeOutAudio(200)
+        .sound()
+            .file(wooshSound2)
+            .delay(900)
+            .fadeOutAudio(200)
+        .sound()
+            .file(wooshSound3)
+            .delay(1300)
+            .fadeOutAudio(200)
+        .sound()
+            .file(wooshSound4)
+            .delay(2700)
+            .fadeOutAudio(200)
+        .sound()
+            .file(wooshSound5)
+            .delay(3100)
+            .fadeOutAudio(200)
+        .sound()
+            .file(wooshSound6)
+            .delay(3500)
+            .fadeOutAudio(200)
+        .effect()
+            .file(puffSideAnimation)
+            .atLocation(token)
+            // @ts-expect-error rotationOffset is correct
+            .rotateTowards(target, { rotationOffset: 180 })
+            .scale(0.7)
+            .delay(600)
+            .fadeIn(100)
+            .opacity(0.3)
+            .fadeOut(200)
+        .effect()
+            .file(puffRingAnimation1)
+            .atLocation(target)
+            .scaleToObject(2.2)
+            .zIndex(1000)
+            .delay(1000)
+            .fadeIn(100)
+            .opacity(0.3)
+            .fadeOut(200)
+        .effect()
+            .file(puffRingAnimation2)
+            .atLocation(token)
+            .belowTokens()
+            .scale(0.7)
+            .delay(3800)
+            .fadeIn(100)
+            .opacity(0.2)
+            .fadeOut(200)
+        .effect()
+            .copySprite(token)
+            .loopProperty("sprite", "rotation", {
+                values: [0, rotationValue],
+                duration: animationTime / 2,
+                pingPong: true,
+                ease: "easeInOutCirc"
+            })
+            .loopProperty("sprite", "position.x", {
+                values: [0, x, 0],
+                duration: animationTime / 2,
+                pingPong: true,
+                gridUnits: false,
+                ease: "easeInOutBack"
+            })
+            .loopProperty("sprite", "position.y", {
+                values: [0, y, 0],
+                duration: animationTime / 2,
+                pingPong: true,
+                gridUnits: false,
+                ease: "easeInOutBack"
+            })
+            .duration(animationTime)
+            .waitUntilFinished(-250)
         .animation()
-        .on(token)
-        .opacity(0)
-        .fadeIn(200)
-        .duration(animationTime)
-        .sound()
-        .file(wooshSound1)
-        .delay(500)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound2)
-        .delay(900)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound3)
-        .delay(1300)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound4)
-        .delay(2700)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound5)
-        .delay(3100)
-        .fadeOutAudio(200)
-        .sound()
-        .file(wooshSound6)
-        .delay(3500)
-        .fadeOutAudio(200)
-        .effect()
-        .file(puffSideAnimation)
-        .atLocation(token)
-        // @ts-expect-error rotationOffset is correct
-        .rotateTowards(target, { rotationOffset: 180 })
-        .scale(0.7)
-        .delay(600)
-        .fadeIn(100)
-        .opacity(0.3)
-        .fadeOut(200)
-        .effect()
-        .file(puffRingAnimation1)
-        .atLocation(target)
-        .scaleToObject(2.2)
-        .zIndex(1000)
-        .delay(1000)
-        .fadeIn(100)
-        .opacity(0.3)
-        .fadeOut(200)
-        .effect()
-        .file(puffRingAnimation2)
-        .atLocation(token)
-        .belowTokens()
-        .scale(0.7)
-        .delay(3800)
-        .fadeIn(100)
-        .opacity(0.2)
-        .fadeOut(200)
-        .effect()
-        .copySprite(token)
-        .loopProperty("sprite", "rotation", {
-            values: [0, rotationValue],
-            duration: animationTime / 2,
-            pingPong: true,
-            ease: "easeInOutCirc"
-        })
-        .loopProperty("sprite", "position.x", {
-            values: [0, x, 0],
-            duration: animationTime / 2,
-            pingPong: true,
-            gridUnits: false,
-            ease: "easeInOutBack"
-        })
-        .loopProperty("sprite", "position.y", {
-            values: [0, y, 0],
-            duration: animationTime / 2,
-            pingPong: true,
-            gridUnits: false,
-            ease: "easeInOutBack"
-        })
-        .duration(animationTime)
-        .waitUntilFinished(-250)
-        .animation()
-        .on(token)
-        .opacity(1)
-        .fadeIn(200)
-        .duration(250);
+            .on(token)
+            .opacity(1)
+            .fadeIn(200)
+            .duration(250);
     sequence.play();
     await delay(animationTime);
 }
